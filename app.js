c84a9bab801c479e597d14df38f3dfd4f6394a21
Jonah Nisenson <jsnisenson@gmail.com>
2015-08-31 14:59:37 -0500

    add package.json scipt

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var express        = require('express'),
     methodOverride = require('method-override'),
     errorHandler   = require('errorhandler'),
     config         = require('./config'),
-    routes         = require('./routes');
+    routes         = require('./routes'),
+    testData       = require('./test-data');
 
 
 
@@ -32,11 +33,22 @@ app
   .use(logger('dev'))
   .use(bodyParser())
   .use(methodOverride())
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(routes.indexRouter)
-  .use(function (req, res) {
-    res.status(404).render('404', {title: 'Not Found :('});
-  });
+  .use(express.static(path.join(__dirname, 'public')));
+
+app.get('/products', function(req, res) {
+  console.log('products is being logged');
+  res.send(200, testData.products);
+});
+
+app.get('/columns', function(req, res) {
+  console.log('products is being logged');
+  res.send(200, testData.columns);
+});
+
+app.get('/groups', function(req, res) {
+  console.log('products is being logged');
+  res.send(200, testData.groups);
+});
 
 if (app.get('env') === 'development') {
   app.use(errorHandler());
@@ -46,7 +58,36 @@ app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+//////////////
 
 /*
-module.exports = app;
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var logger = require('morgan');
+var request = require('request');
+
+var rootFolder = process.env.SALLYROOT || '/dist';
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + rootFolder));
+
+require('./server/routes')(app);
+
+require('./server/controllers/checkr/checkr');
+
+var server = app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
 */
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,11 @@ var testData = require('./../test-data');
 /**
  * the new Router exposed in express 4
  * the indexRouter handles all requests to the `/` path
- */
 var indexRouter = express.Router();
 
-/**
+/!**
  * this accepts all request methods to the `/` path
- */
+ *!/
 
 indexRouter.route('/')
   .all(function (req, res) {
@@ -23,6 +22,7 @@ indexRouter.route('/')
 
 indexRouter.route('/products')
   .get(function (req, res) {
+    console.log('products is being logged');
     res.send(200, testData.products);
   });
 
@@ -37,4 +37,27 @@ indexRouter.route('/groups')
   });
 
 
-exports.indexRouter = indexRouter;
+exports.indexRouter = indexRouter;*/
+
+
+module.exports = function(app) {
+
+
+  app.get('/products', function(req, res) {
+    console.log('products is being logged');
+    res.send(200, testData.products);
+  });
+
+  app.get('/columns', function(req, res) {
+    console.log('products is being logged');
+    res.send(200, testData.columns);
+  });
+
+  app.get('/groups', function(req, res) {
+    console.log('products is being logged');
+    res.send(200, testData.groups);
+  });
+
+
+
+};
